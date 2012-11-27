d9133b7c3cb52e1a202ea0ca6eb2150467458315
Todd Lyon <toddlyon@iastate.edu>
2012-11-27 10:56:22 -0600

    added hits needed

--- HammerRace/HammerRace/MainViewController.m
+++ HammerRace/HammerRace/MainViewController.m
@@ -10,6 +10,7 @@
 #import <QuartzCore/QuartzCore.h>
 // This is defined in Math.h
 #define M_PI   3.14159265358979323846264338327950288   /* pi */
+#define HITS_NEEDED 50
 
 // Our conversion definition
 #define DEGREES_TO_RADIANS(angle) (angle / 180.0 * M_PI)
